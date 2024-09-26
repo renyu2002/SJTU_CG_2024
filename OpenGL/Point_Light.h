feda030cd9abce864e63082bc36185f0a99bbc03
YuRen <77331416+renyu2002@users.noreply.github.com>
2024-09-27 00:44:15 +0800

    Simple Scene

---
+++ OpenGL/Point_Light.h
@@ -0,0 +1,93 @@
+#pragma once
+#include "Shader.h"
+#include <GL\glew.h>				// 根据自身情况进行设置
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+
+GLfloat vertices_2[] =
+{	// 坐标
+    // x、y、z 坐标				// color
+    -0.5f, -0.5f, -0.5f,		1.0f, 0.0f, 0.0f,	// red 红色面
+     0.5f, -0.5f, -0.5f,		1.0f, 0.0f, 0.0f,
+     0.5f,  0.5f, -0.5f,		1.0f, 0.0f, 0.0f,
+     0.5f,  0.5f, -0.5f,		1.0f, 0.0f, 0.0f,
+    -0.5f,  0.5f, -0.5f,		1.0f, 0.0f, 0.0f,
+    -0.5f, -0.5f, -0.5f,		1.0f, 0.0f, 0.0f,
+
+    -0.5f, -0.5f,  0.5f,		0.0f, 1.0f, 0.0f,	// green 绿色面
+     0.5f, -0.5f,  0.5f,		0.0f, 1.0f, 0.0f,
+     0.5f,  0.5f,  0.5f,		0.0f, 1.0f, 0.0f,
+     0.5f,  0.5f,  0.5f,		0.0f, 1.0f, 0.0f,
+    -0.5f,  0.5f,  0.5f,		0.0f, 1.0f, 0.0f,
+    -0.5f, -0.5f,  0.5f,		0.0f, 1.0f, 0.0f,
+
+    -0.5f,  0.5f,  0.5f,		0.0f, 0.0f, 1.0f,	// blue 蓝色面
+    -0.5f,  0.5f, -0.5f,		0.0f, 0.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f,		0.0f, 0.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f,		0.0f, 0.0f, 1.0f,
+    -0.5f, -0.5f,  0.5f,		0.0f, 0.0f, 1.0f,
+    -0.5f,  0.5f,  0.5f,		0.0f, 0.0f, 1.0f,
+
+     0.5f,  0.5f,  0.5f,		1.0f, 1.0f, 0.0f,	// yellow 黄色面
+     0.5f,  0.5f, -0.5f,		1.0f, 1.0f, 0.0f,
+     0.5f, -0.5f, -0.5f,		1.0f, 1.0f, 0.0f,
+     0.5f, -0.5f, -0.5f,		1.0f, 1.0f, 0.0f,
+     0.5f, -0.5f,  0.5f,		1.0f, 1.0f, 0.0f,
+     0.5f,  0.5f,  0.5f,		1.0f, 1.0f, 0.0f,
+
+    -0.5f, -0.5f, -0.5f,		1.0f, 0.0f, 1.0f,	// purple 紫色面
+     0.5f, -0.5f, -0.5f,		1.0f, 0.0f, 1.0f,
+     0.5f, -0.5f,  0.5f,		1.0f, 0.0f, 1.0f,
+     0.5f, -0.5f,  0.5f,		1.0f, 0.0f, 1.0f,
+    -0.5f, -0.5f,  0.5f,		1.0f, 0.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f,		1.0f, 0.0f, 1.0f,
+
+    -0.5f,  0.5f, -0.5f,		0.0f, 1.0f, 1.0f,	// cyan 青色面
+     0.5f,  0.5f, -0.5f,		0.0f, 1.0f, 1.0f,
+     0.5f,  0.5f,  0.5f,		0.0f, 1.0f, 1.0f,
+     0.5f,  0.5f,  0.5f,		0.0f, 1.0f, 1.0f,
+    -0.5f,  0.5f,  0.5f,		0.0f, 1.0f, 1.0f,
+    -0.5f,  0.5f, -0.5f,		0.0f, 1.0f, 1.0f,
+
+};
+
+class Point_Light
+{
+public:
+	Point_Light()				// 构造函数
+	{
+		this->update();
+	}
+
+	void Draw(Shader& shader)	// 绘制函数
+	{
+		glBindVertexArray(light_VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+		glBindVertexArray(0);
+	}
+
+	~Point_Light()				// 析构函数
+	{
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		glBindVertexArray(0);
+		glDeleteVertexArrays(1, &light_VAO);
+		glDeleteBuffers(1, &light_VBO);
+	}
+
+private:
+	GLuint light_VAO, light_VBO;
+	void update()
+	{
+		/* 设置顶点缓冲对象(VBO) + 设置顶点数组对象(VAO) */
+		glGenVertexArrays(1, &light_VAO);
+		glGenBuffers(1, &light_VBO);
+		glBindVertexArray(light_VAO);
+		glBindBuffer(GL_ARRAY_BUFFER, light_VBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices_2), vertices_2, GL_STATIC_DRAW);
+
+		/* 设置链接顶点属性 */
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+		glEnableVertexAttribArray(0);
+	}
+};
+
